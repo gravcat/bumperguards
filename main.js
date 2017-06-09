4a6029b711485b95105f3fb460ce7127424cdf26
Nick Thieling <nick@adrift.io>
2017-06-09 00:02:14 -0500

    support to get extra data and new safe functions

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 // balances-lg is the element that holds the bal .innerText accesses the object and gets the value inside that, which is the integer
-var readBal = document.getElementById("balances-lg").innerText // will register int like "0.16201530"
-readBal = readBal.replace(/\s/g, ""); // data massaging to remove newlines
+var readBal = document.getElementById("balances-lg").innerText
+readBal = readBal.replace(/\s/g, "");
 
-// below this, we'll stop the bets
-var thresholdBal = .110
+// won-bet is the element that stores what the change to your balance is as a result of the play, the "floating" number that pops on each bet
+var betAmount = document.getElementById("won-bet").innerText
+
+// set var to hold whether or not we're trying to stop
+var stopping = 0
+
+// customizable 
+var thresholdBal = .06
+//var thresholdBet = 0.00001200
+var thresholdBet = -0.00000160
 
 // Wrapping to interface with the user and get/check parameters
 console.log("Have the game STOPPED and on AUTOMATED mode. To stop the output/loop, execute window.clearInterval(intervalCheck) or close the tab")
@@ -17,24 +25,42 @@ if (beginBal != readBal) {
 
 // potential to lose monies, let's be really explicit and check prompted vs read to make sure on the call to begin as well
 if (beginBal = readBal && readBal > thresholdBal) {
-    document.getElementById('btn-bet-start-pilot-dice').click(); // if balance checks out, click the "start"
+    document.getElementById('btn-bet-start-pilot-dice').click();
 }
 
-// print out the balance we read, just for the sake of logging iterations
+// print out the balance we read
 console.log("Starting balance: " + readBal);
-
+    
 // outputs current balance
 function check(readBal) {
 	var readBal = document.getElementById("balances-lg").innerText
-    
+    var betAmount = document.getElementById("history-my-bets-dice").rows[0].cells[7].innerText
+
     if (readBal > thresholdBal) {
-        console.log("Balance: " + readBal, "Threshold: " + thresholdBal);
+        //console.log("Balance: " + readBal, "Threshold: " + thresholdBal);
         var nowBal = readBal
     }
-    if (readBal < thresholdBal) {
+
+    if (thresholdBet > betAmount && stopping == 0) {
+        var stopping = 1
+        console.log("Surpassed bet stop threshold with amount " + betAmount)
         document.getElementById('btn-bet-stop-pilot-dice').click();
+        setTimeout(function () {
+            document.getElementById('btn-bet-start-pilot-dice').click();
+            var stopping = 0
+        }, 1000);
+    }
+
+    if (readBal < thresholdBal && stopping == 0) {
+        var stopping = 1
+        setTimeout(function () {
+            document.getElementById('btn-bet-start-pilot-dice').click();
+            var stopping = 0
+        }, 1000);
         console.log("Balance dropped below threshold! " + thresholdBal);
     }
 }
 
 var intervalCheck = setInterval(check, 1000, readBal);
+
+//
